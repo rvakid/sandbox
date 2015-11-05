13a4cadc61c2714332e9d8a9f983993554b8b687
rvakid <brad@penupdesigns.com>
2015-11-05 10:02:24 -0800

    added date to footer

--- wp/wp-content/themes/beckerbase/footer.php
+++ wp/wp-content/themes/beckerbase/footer.php
@@ -1,7 +1,7 @@
 
 	<footer>
 		<div class="col-md-12">
-        <p class="text-center">Here is text in the footer</p>
+        <p class="text-center">Copyright &copy; <?php the_date('Y'); ?> $Site Name$ </p>
         </div>
 	</footer>
 	
